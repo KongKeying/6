f964889b9a67bcb59a031b06bd1d80aaf8ff861d
KongKeying <134877746+KongKeying@users.noreply.github.com>
2023-05-29 02:44:02 +0800

    Add files via upload

---
+++ hii.py
@@ -0,0 +1,5 @@
+firstNum = int(input("Enter First Number: "))
+secondNum = int(input("Enter Second Number: "))
+
+total= firstNum + secondNum
+print("Total of", firstNum,"and", secondNum,"is", total)
